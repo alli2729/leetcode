7f2354c83011be7b9dd37a41a9177c63b546ca70
Ali Karimi <a.karimi2729@gmail.com>
2024-07-24 03:14:05 +0330

    Majority Element

---
+++ Majority Element.dart
@@ -0,0 +1,11 @@
+/*
+leetcode.com
+*169. Majority Element
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+*/
+int majorityElement(List<int> nums) {
+  nums.sort();
+  return nums[nums.length ~/ 2];
+}
